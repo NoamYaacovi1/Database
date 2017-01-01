bc8e5d650151938bce179c9536e7ac18e1244302
NoamYaacovi1 <noamyaacovi@gmail.com>
2017-01-01 17:46:28 +0200

    Starting DateBase(not working)

---
+++ Database Ex/Database Ex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Database_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C: \\Users\\Bar\\Documents\\GitHub\\Database\\Database Ex\\Database Ex\\Database1.mdf\";Integrated Security=True";
+            SqlConnection conn = new SqlConnection(s);
+            conn.Open();
+
+            /**/SqlCommand createTableCommand = new SqlCommand();
+            createTableCommand.Connection = conn;
+            createTableCommand.CommandText = "CREATE TABLE songs (Singer varchar(255), name varchar(255), year int);";
+            createTableCommand.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
